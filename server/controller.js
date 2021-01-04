bc5ddd4f8f16c1c6bd7604a6d6deea17a9b94355
rachelseaborn <raseaborn@gmail.com>
2021-01-03 22:35:15 -0700

    server updates

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,18 @@
-module.exports = {
 
+const data = require('../src/data.json')
+
+//Put server handler functions in controller files. Keep separate from server logic. 
+//Handler functions handle the server response to a client request
+//module.exports allows function to be exported as an object
+module.exports = {
+    getRecipes: (req, res) => {
+        res.status(200).send(data)
+    },
+
+    getSingleRecipe: (req, res) => {
+        const { id } = req.params  //destructure id off of req.params
+        const recipe = data.find(el => +id === el.id)
+        if (!recipe) return res.sendStatus(404)
+        res.status(200).send(recipe)
+    }
 }
--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
+
 //import express
 const express = require('express');
-//invoke express to access its functionality. Giant object with properties for server management 
+//invoke express to access its framework functionality. Giant object with properties for server management 
 const app = express();
+//app.use(express.json());
+
+const ctrl = require('./controller');
+const data = require('../src/data.json');
 const port = 3713;
 
-//endpoints
-app.get('/api/recipes')
+//app.use(express.json)
+//endpoints - gateways to server data. Define how client can request data from server.
+
+app.get('/api/recipes', ctrl.getRecipes)
+
+//get needs a cb. Err may say 'route.get...'
+//If using a query or body, don't need to add anything. Syntax below for using params.
+//browser testing ex: localhost:3713/api/recipe/2
+
+//app.get('/api/recipe/:id', ctrl.getSingleRecipe)
 
 
-//Fn that takes 2 args; put at bottom 
+//Fn that takes 2 args: 1) port to listen to, 2) cb function. Put below endpoints.
+
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
